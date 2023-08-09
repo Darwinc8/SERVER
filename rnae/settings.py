ed3f72f44116d9f7eb769b93b3d2dadb9231f7a5
Darwinc <darwincspace@gmail.com>
2023-08-09 13:04:04 -0600

    mas ajustes de trans

--- rnae/settings.py
+++ rnae/settings.py
@@ -121,7 +121,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'es-mx'
+LANGUAGE_CODE = 'en'
 TIME_ZONE = 'America/Mexico_City'
 
 USE_L10N = True
