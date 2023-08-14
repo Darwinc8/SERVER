22263ebc6fb4549aa3037c8a6cdbbc925c278d1f
Darwinc <darwincspace@gmail.com>
2023-08-14 10:14:41 -0600

    modificacion de la tabla armamento

--- armamento/models.py
+++ armamento/models.py
@@ -4,40 +4,40 @@ from catalogos.models import Institucion, Dependencia, Calibre, Edo_conservacion
 # Create your models here.
 class Armamento(models.Model):
     ID_ALTERNA= models.AutoField(primary_key=True)
-    ID_ARMA = models.DecimalField(max_digits=10, decimal_places=0, unique=True)
-    INSTITUCION = models.ForeignKey(Institucion, on_delete=models.RESTRICT)
-    DEPENDENCIA = models.ForeignKey(Dependencia, on_delete=models.RESTRICT)
-    ENTIDAD = models.ForeignKey(Entidad, on_delete=models.RESTRICT)
+    ID_ARMA = models.DecimalField(max_digits=10, decimal_places=0, unique=True, null=False, blank=False)
+    INSTITUCION = models.ForeignKey(Institucion, on_delete=models.RESTRICT, null=False, blank=False)
+    DEPENDENCIA = models.ForeignKey(Dependencia, on_delete=models.RESTRICT, null=False, blank=False)
+    ENTIDAD = models.ForeignKey(Entidad, on_delete=models.RESTRICT, null=False, blank=False)
     
     MUNICIPIO = models.ForeignKey(
         Municipio,
         on_delete=models.CASCADE,
-        to_field='id'
+        to_field='id', null=False, blank=False
     )
     
-    NUMERO_LOC = models.ForeignKey(LOC, on_delete=models.RESTRICT)
-    FOLIO_C = models.CharField(max_length=20)
-    FOLIO_D = models.CharField(max_length=20)
-    CLASE_TIPO_ARMA = models.ForeignKey(Tipo, on_delete=models.RESTRICT)
-    CALIBRE_ARMA = models.ForeignKey(Calibre, on_delete=models.RESTRICT)
-    MARCA_ARMA = models.ForeignKey(Marca, on_delete=models.RESTRICT)
-    MODELO_ARMA = models.ForeignKey(Modelo, on_delete=models.RESTRICT)
-    MATRICULA = models.CharField(max_length=20)
-    MATRICULA_CANON = models.CharField(max_length=40, blank=True)
-    FECHA = models.DateField()
-    FECHA_LOC = models.DateField()
-    ESTADO_ARMA = models.ForeignKey(Edo_conservacion, on_delete=models.RESTRICT)
-    FECHA_CAPTURA = models.DateField()
-    OBSERVACIONES = models.TextField()
-    ESTATUS_ARMA = models.ForeignKey(Estatus_Arma, on_delete=models.RESTRICT)
-    CUIP_PORTADOR = models.CharField(max_length=20)
-    CUIP_RESPONSABLE = models.CharField(max_length=20)
-    CIHB = models.CharField(max_length=20)
-    FECHA_BAJA_LOGICA = models.DateField()
-    MOTIVO_BAJA = models.TextField()
-    DOCUMENTO_BAJA = models.CharField(max_length=20)
-    OBSERVACIONES_BAJA = models.TextField()
-    FECHA_BAJA_DOCUMENTO = models.DateField()
+    NUMERO_LOC = models.ForeignKey(LOC, on_delete=models.RESTRICT, null=False, blank=False)
+    FOLIO_C = models.CharField(max_length=20, null=False, blank=False)
+    FOLIO_D = models.CharField(max_length=20, null=False, blank=False)
+    CLASE_TIPO_ARMA = models.ForeignKey(Tipo, on_delete=models.RESTRICT, null=False, blank=False)
+    CALIBRE_ARMA = models.ForeignKey(Calibre, on_delete=models.RESTRICT, null=False, blank=False)
+    MARCA_ARMA = models.ForeignKey(Marca, on_delete=models.RESTRICT, null=False, blank=False)
+    MODELO_ARMA = models.ForeignKey(Modelo, on_delete=models.RESTRICT, null=False, blank=False)
+    MATRICULA = models.CharField(max_length=20, null=False, blank=False)
+    MATRICULA_CANON = models.CharField(max_length=40, null=True, blank=True)
+    FECHA = models.DateField(null=False, blank=False)
+    FECHA_LOC = models.DateField(null=False, blank=False)
+    ESTADO_ARMA = models.ForeignKey(Edo_conservacion, on_delete=models.RESTRICT, null=False, blank=False)
+    FECHA_CAPTURA = models.DateField(null=False, blank=False)
+    OBSERVACIONES = models.TextField(null=False, blank=False)
+    ESTATUS_ARMA = models.ForeignKey(Estatus_Arma, on_delete=models.RESTRICT, null=False, blank=False)
+    CUIP_PORTADOR = models.CharField(max_length=20, null=False, blank=False)
+    CUIP_RESPONSABLE = models.CharField(max_length=20, null=False, blank=False)
+    CIHB = models.CharField(max_length=20, null=True, blank=True)
+    FECHA_BAJA_LOGICA = models.DateField(null=True, blank=True)
+    MOTIVO_BAJA = models.TextField(null=True, blank=True)
+    DOCUMENTO_BAJA = models.CharField(max_length=20, null=True, blank=True)
+    OBSERVACIONES_BAJA = models.TextField(null=True, blank=True)
+    FECHA_BAJA_DOCUMENTO = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.ID_ARMA}-{self.CUIP_PORTADOR}-{self.CUIP_RESPONSABLE}"
--- sesions/urls.py
+++ sesions/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from django.views.i18n import set_language
+
 urlpatterns = [
     path('registrarse/',views.registrarse, name='registrarse'),
     path('iniciar_sesion/',views.iniciar_sesion, name='iniciar_sesion'),
