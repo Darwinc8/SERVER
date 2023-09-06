11d2fae941fc146665bf553b15de3417f08ba00f
Darwinc <darwincspace@gmail.com>
2023-09-06 10:41:22 -0600

    registro de PortadorLog

--- portadores/admin.py
+++ portadores/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Portador
+from .models import Portador, PortadorLog
 # Register your models here.
 admin.site.register(Portador)
+admin.site.register(PortadorLog)
