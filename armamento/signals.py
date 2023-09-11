34c5a004edd473709513022601e8342bc9da8493
Darwinc <darwincspace@gmail.com>
2023-09-11 13:58:16 -0600

    signal de eliminacion de armamento

--- armamento/signals.py
+++ armamento/signals.py
@@ -1,7 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from armamento.models import Armamento, ArmamentoLog
-import os, shutil
 
 @receiver(post_save, sender=Armamento)
 def mi_funcion_despues_de_insert(sender, instance, created, **kwargs):
@@ -44,4 +43,42 @@ def mi_funcion_despues_de_insert(sender, instance, created, **kwargs):
         usuario = instance.usuario,
         ultima_modificacion = instance.ultima_modificacion
     )
-    print("Copia creada...")
+    print("Copia de creación/Modificacíon creada...")
+
+@receiver(post_save, sender=Armamento)
+def mi_funcion_antes_de_eliminar(sender, instance,**kwargs):
+    ArmamentoLog.objects.create(
+        estado = "Eliminación",
+        id_alterna = instance,
+        id_arma = instance.ID_ARMA,
+        institucion = instance.INSTITUCION,
+        dependencia = instance.DEPENDENCIA,
+        entidad = instance.ENTIDAD,
+        municipio = instance.MUNICIPIO,
+        numero_loc = instance.NUMERO_LOC,
+        folio_c= instance.FOLIO_C,
+        folio_d = instance.FOLIO_D,
+        clase_tipo_arma = instance.CLASE_TIPO_ARMA,
+        calibre_arma = instance.CALIBRE_ARMA,
+        marca_arma = instance.MARCA_ARMA,
+        modelo_arma = instance.MODELO_ARMA,
+        matricula = instance.MATRICULA,
+        matricula_canon = instance.MATRICULA_CANON,
+        fecha = instance.FECHA,
+        fecha_loc = instance.FECHA_LOC,
+        estado_arma = instance.ESTADO_ARMA,
+        fecha_captura = instance.FECHA_CAPTURA,
+        observaciones = instance.OBSERVACIONES,
+        estatus_arma = instance.ESTATUS_ARMA,
+        cuip_portador = instance.CUIP_PORTADOR.CUIP,
+        cuip_responsable = instance.CUIP_RESPONSABLE.CUIP,
+        cihb = instance.CIHB,
+        fecha_baja_logica = instance.FECHA_BAJA_LOGICA,
+        motivo_baja = instance.MOTIVO_BAJA,
+        documento_baja = instance.DOCUMENTO_BAJA,
+        observaciones_baja = instance.OBSERVACIONES_BAJA,
+        fecha_baja_documento = instance.FECHA_BAJA_DOCUMENTO,
+        usuario = instance.usuario,
+        ultima_modificacion = instance.ultima_modificacion
+    )
+    print("Copia de eliminación creada...")
