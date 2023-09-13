d31ac923e136a61a84b80deadf50fe483bc76500
Darwinc <darwincspace@gmail.com>
2023-09-13 12:56:26 -0600

    cambios en error 404

--- rnae/urls.py
+++ rnae/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 from catalogos import views
 from django.conf import settings
 from django.contrib.staticfiles.urls import static
+from utilidades.views import error_404_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,6 +16,8 @@ urlpatterns = [
     path('imagenes/', include('imagenes.urls')),
     path('error/', include('utilidades.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = error_404_view
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
