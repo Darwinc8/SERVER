b1e407ceae4ec1cca9c2697f1c4e33518d61b449
Darwinc <darwincspace@gmail.com>
2023-09-29 13:48:49 -0600

    update en registrarse vista

--- sesions/urls.py
+++ sesions/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
@@ -7,3 +9,6 @@ urlpatterns = [
     path('cerrar_sesion/',views.cerrar_sesion, name='cerrar_sesion'),
     path('cambiar_lenguaje/<str:language_code>/', views.cambiar_lenguaje, name='cambiar_lenguaje')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
