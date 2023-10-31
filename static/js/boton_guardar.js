c55321473708f4ea540e872c63837164b74bc442
Darwinc <darwincspace@gmail.com>
2023-10-31 13:03:58 -0600

    enter y dif de campos en imagenes

--- static/js/boton_guardar.js
+++ static/js/boton_guardar.js
@@ -1,32 +1,39 @@
-$(document).ready(function() {
+
+  $(document).ready(function() {
     // Almacenar los valores iniciales de los campos del formulario
     var initialData = {};
-    $('#myForm input[type="text"], #myForm input[type="email"], #myForm input[type="file"]').each(function() {
-      initialData[this.name] = $(this).val();
+    $('#myForm input[type="text"], #myForm input[type="number"], #myForm select, #myForm input[type="file"]').each(function() {
+      var fieldName = $(this).attr('name');
+      console.log("Selected field name: ", fieldName);
+      initialData[fieldName] = $(this).val();
     });
-  
+
     // Función para comprobar si ha habido cambios en los campos
     function hasChanges() {
       var formData = {};
-      $('#myForm input[type="text"], #myForm input[type="email"], #myForm input[type="file"]').each(function() {
-        formData[this.name] = $(this).val();
+      $('#myForm input[type="text"], #myForm input[type="number"], #myForm select, #myForm input[type="file"]').each(function() {
+        var fieldName = $(this).attr('name');
+        console.log("Selected field name: ", fieldName);
+        formData[fieldName] = $(this).val();
       });
-  
+
       return JSON.stringify(initialData) !== JSON.stringify(formData);
     }
-  
+
     // Habilitar/deshabilitar el botón de guardar según los cambios
     function toggleGuardarButton() {
       if (hasChanges()) {
-        $('#guardar-btn').removeClass('disabled');
+        $('#guardar-btn').prop('disabled', false);
       } else {
-        $('#guardar-btn').addClass('disabled');
+        $('#guardar-btn').prop('disabled', true);
       }
     }
-  
+
     // Escuchar los eventos de cambio en los campos del formulario
-    $('#myForm input[type="text"], #myForm input[type="email"], #myForm input[type="file"]').on('input', toggleGuardarButton);
-  
+    $('#myForm input[type="text"], #myForm input[type="number"]').on('input', toggleGuardarButton);
+    $('#myForm select').on('change', toggleGuardarButton);
+    $('#myForm input[type="file"]').on('change', toggleGuardarButton);
+
     // Inicialmente, deshabilitar el botón de guardar si no hay cambios
     toggleGuardarButton();
   });
