1df5e4e2760d08c0ba8656ac5f4e9d42a67bdcc0
Darwinc <darwincspace@gmail.com>
2023-10-31 13:08:55 -0600

    impl a text area y date

--- static/js/boton_guardar.js
+++ static/js/boton_guardar.js
@@ -1,39 +1,38 @@
+$(document).ready(function() {
+  // Almacenar los valores iniciales de los campos del formulario
+  var initialData = {};
+  $('#myForm input[type="text"], #myForm input[type="number"], #myForm select, #myForm input[type="file"], #myForm textarea, #myForm input[type="date"]').each(function() {
+    var fieldName = $(this).attr('name');
+    console.log("Selected field name: ", fieldName);
+    initialData[fieldName] = $(this).val();
+  });
 
-  $(document).ready(function() {
-    // Almacenar los valores iniciales de los campos del formulario
-    var initialData = {};
-    $('#myForm input[type="text"], #myForm input[type="number"], #myForm select, #myForm input[type="file"]').each(function() {
+  // Función para comprobar si ha habido cambios en los campos
+  function hasChanges() {
+    var formData = {};
+    $('#myForm input[type="text"], #myForm input[type="number"], #myForm select, #myForm input[type="file"], #myForm textarea, #myForm input[type="date"]').each(function() {
       var fieldName = $(this).attr('name');
       console.log("Selected field name: ", fieldName);
-      initialData[fieldName] = $(this).val();
+      formData[fieldName] = $(this).val();
     });
 
-    // Función para comprobar si ha habido cambios en los campos
-    function hasChanges() {
-      var formData = {};
-      $('#myForm input[type="text"], #myForm input[type="number"], #myForm select, #myForm input[type="file"]').each(function() {
-        var fieldName = $(this).attr('name');
-        console.log("Selected field name: ", fieldName);
-        formData[fieldName] = $(this).val();
-      });
+    return JSON.stringify(initialData) !== JSON.stringify(formData);
+  }
 
-      return JSON.stringify(initialData) !== JSON.stringify(formData);
+  // Habilitar/deshabilitar el botón de guardar según los cambios
+  function toggleGuardarButton() {
+    if (hasChanges()) {
+      $('#guardar-btn').prop('disabled', false);
+    } else {
+      $('#guardar-btn').prop('disabled', true);
     }
+  }
 
-    // Habilitar/deshabilitar el botón de guardar según los cambios
-    function toggleGuardarButton() {
-      if (hasChanges()) {
-        $('#guardar-btn').prop('disabled', false);
-      } else {
-        $('#guardar-btn').prop('disabled', true);
-      }
-    }
+  // Escuchar los eventos de cambio en los campos del formulario
+  $('#myForm input[type="text"], #myForm input[type="number"], #myForm textarea, #myForm input[type="date"]').on('input change', toggleGuardarButton);
+  $('#myForm select').on('change', toggleGuardarButton);
+  $('#myForm input[type="file"]').on('change', toggleGuardarButton);
 
-    // Escuchar los eventos de cambio en los campos del formulario
-    $('#myForm input[type="text"], #myForm input[type="number"]').on('input', toggleGuardarButton);
-    $('#myForm select').on('change', toggleGuardarButton);
-    $('#myForm input[type="file"]').on('change', toggleGuardarButton);
-
-    // Inicialmente, deshabilitar el botón de guardar si no hay cambios
-    toggleGuardarButton();
-  });
+  // Inicialmente, deshabilitar el botón de guardar si no hay cambios
+  toggleGuardarButton();
+});
