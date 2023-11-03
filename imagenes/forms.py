febf9856674f067c26d74c1c2c97329136073cd8
Darwinc <darwincspace@gmail.com>
2023-11-03 13:51:11 -0600

    cambio nombre clase imagen

--- imagenes/forms.py
+++ imagenes/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-from .models import Imagenes, Institucion, Entidad, Dependencia, Tipo_Imagen, Armamento
+from .models import  Institucion, Entidad, Dependencia, Tipo_Imagen, Armamento, Imagenes
 
-class CrearImagenForm(forms.ModelForm):
+class ImagenForm(forms.ModelForm):
     class Meta:
         model = Imagenes
         fields = [
@@ -46,29 +46,4 @@ class BusquedaImagenesForm(forms.Form):
         initial='ID_ARMA__ID_ARMA__icontains'
         )    
 
-class EditarImagenForm(forms.ModelForm):
-    class Meta:
-        model = Imagenes
-        fields = [
-            'ID_ARMA',
-            'INSTITUCION',
-            'ENTIDAD',
-            'DEPENDENCIA',
-            'IMAKEY',
-            'DESIMA',
-            'TIPO',
-            'FOLIO',
-            'GRUPO',
-            'IMAGEN'
-            ]
-        
-    ID_ARMA = forms.ModelChoiceField(queryset=Armamento.objects.all(),to_field_name='MATRICULA',label='Matricula')
-    
-    INSTITUCION = forms.ModelChoiceField(queryset=Institucion.objects.all(),to_field_name='ID_INSTITUCION',label='Institucion')
-    
-    ENTIDAD = forms.ModelChoiceField(queryset=Entidad.objects.all(),to_field_name='ID_ENTIDAD',label='Entidad')
-    
-    DEPENDENCIA = forms.ModelChoiceField(queryset=Dependencia.objects.all().order_by('DEPENDENCIA'),to_field_name='ID_DEPENDENCIA',label='Dependencia')
-    
-    TIPO = forms.ModelChoiceField(queryset=Tipo_Imagen.objects.all().order_by('DESCRIPCION'),to_field_name='ID_IMAGEN',label='Tipo de Imagen')
             
--- imagenes/views.py
+++ imagenes/views.py
@@ -2,7 +2,7 @@ import os
 from django.shortcuts import get_object_or_404, render, redirect
 from .models import Imagenes
 from django.contrib.auth.decorators import login_required
-from .forms import CrearImagenForm, EditarImagenForm
+from .forms import ImagenForm
 from catalogos.models import Institucion
 from django.http import JsonResponse
 from .forms import BusquedaImagenesForm
@@ -24,12 +24,12 @@ def imagenes(request):
 @login_required
 def crear_imagen(request):
     if request.method == 'GET':
-        form = CrearImagenForm(request.POST or None)
+        form = ImagenForm(request.POST or None)
         return render(request, 'crear_imagen.html', {
             'form': form
         })
     else:
-        form = CrearImagenForm(request.POST, request.FILES)
+        form = ImagenForm(request.POST, request.FILES)
         if form.is_valid():
             objecto = form.save(commit=False)
             objecto.usuario = request.user
@@ -66,7 +66,7 @@ def editar_imagen(request, id):
         imagen = get_object_or_404(Imagenes,pk=id)
         ruta = f"/RNAE_V1{imagen.IMAGEN.url}"
         
-        form = EditarImagenForm(request.POST or None, request.FILES or None, instance=imagen)
+        form = ImagenForm(request.POST or None, request.FILES or None, instance=imagen)
 
         if form.is_valid() and request.method == 'POST':
             if 'IMAGEN' in form.changed_data:
