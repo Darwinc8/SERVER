845adbab02ff7b7c4c5a0c37cd1b1cd6e5d99c83
Darwinc <darwincspace@gmail.com>
2023-11-06 20:54:35 -0600

    captcha

--- sesions/forms.py
+++ sesions/forms.py
@@ -1,7 +1,9 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from captcha.fields import CaptchaField
 
 class UserCreationForm(UserCreationForm):
     captcha = CaptchaField()
 
+class AuthenticationForm(AuthenticationForm):
+    captcha = CaptchaField()
--- sesions/views.py
+++ sesions/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm
-from .forms import UserCreationForm
+from .forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
@@ -49,20 +48,23 @@ def cerrar_sesion(request):
     return redirect('home')
 
 def iniciar_sesion(request):
-    if request.method == 'GET':
-        return render(request, 'sign_in.html',{
-        'form': AuthenticationForm
-        })
-    else:
-        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
-        if user is None:
-            return render(request, 'sign_in.html',{
-            'form': AuthenticationForm,
-            'error': 'Usuario o Contraseña incorrecta'
-            })
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                # Usuario no encontrado
+                return render(request, 'sign_in.html', {'form': form, 'error': 'Usuario no encontrado. Por favor, inténtalo de nuevo.'})
         else:
-            login(request, user)
-            return redirect('home')  
+            # Error de CAPTCHA o cualquier otro error en el formulario
+            return render(request, 'sign_in.html', {'form': form})
+    else:
+        return render(request, 'sign_in.html', {'form': AuthenticationForm()})
 
 @login_required
 def cambiar_lenguaje(request, language_code):
