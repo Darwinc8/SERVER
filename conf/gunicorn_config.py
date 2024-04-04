04f5a9b60b1eeceaada7b8c5f7043496722ed12d
Darwinc8 <darwincspace@gmail.com>
2024-04-04 11:34:54 -0600

    quitando rastro de las carpetas que no requieren seguir

--- conf/gunicorn_config.py
+++
@@ -1,4 +0,0 @@
-command = '/home/sistemas/RNAE/entorno/bin/gunicorn'
-pythonpath = '/home/sistemas/RNAE'
-bind = '172.16.1.20:8000'
-workers = 3
--- static/js/deshabilitar_matricula.js
+++
@@ -1,9 +0,0 @@
-function enableFieldAndSubmit(event) {
-    event.preventDefault(); // Prevenir el envío del formulario por defecto
-
-    var field = document.getElementById('matricula');
-    field.disabled = false;
-
-    // Envía el formulario después de habilitar el campo
-    document.getElementById('myForm').submit();
-}
