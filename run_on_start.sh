8c452307a5821b3abd518668f435fd5c9f14c17e
Darwinc8 <darwincspace@gmail.com>
2024-05-28 18:29:15 -0600

    correccion del sh

--- run_on_start.sh
+++ run_on_start.sh
@@ -5,7 +5,7 @@ cd /home/sistemas/RNAE/
 
 # Activar el entorno virtual
 source entorno/bin/activate
-
+python3 manage.py collectstatic -yes
 # Ejecutar el servidor Gunicorn
 gunicorn -c /home/sistemas/RNAE/conf/gunicorn_config.py rnae.wsgi
 
