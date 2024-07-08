575ea4ba62a7687ea0e3d0965c6e8b221343d35c
Darwinc8 <darwincspace@gmail.com>
2024-07-08 09:29:56 -0600

    debug false

--- rnae/settings.py
+++ rnae/settings.py
@@ -16,7 +16,7 @@ RECAPTCHA_PUBLIC_KEY = 'TU_CLAVE_DE_SITIO'
 RECAPTCHA_PRIVATE_KEY = 'TU_CLAVE_SECRETA'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['172.16.1.20',
                  '127.0.0.1']
 
